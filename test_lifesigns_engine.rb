d0aa0d0714e7d9af35ed505df346918c58c1769e
William Willis Whinn <w_willis86@hotmail.co.uk>
2017-09-22 16:35:23 +0100

    Update and refine test suite

---
+++ test_lifesigns_engine.rb
@@ -0,0 +1,49 @@
+#!/usr/bin/env ruby
+# Script Name: test_lifesigns_engine.rb
+# Script Author: William Willis Whinn
+# Script Created: 2017-09-22
+# Script Updated: 2017-09-22
+# Project Repository: https://github.com/ultraviolet-1986/lifesigns-engine-ruby
+
+# encoding: utf-8
+
+##########
+# System #
+##########
+
+#require_relative 'lifesigns_engine/system/test_inventory'
+#require_relative 'lifesigns_engine/system/test_style_sheet'
+#require_relative 'lifesigns_engine/system/test_terminal'
+
+##############
+# Characters #
+##############
+
+#require_relative 'lifesigns_engine/characters/test_humanoid'
+
+#############
+# Locations #
+#############
+
+#require_relative 'lifesigns_engine/locations/test_location'
+
+###########
+# Objects #
+###########
+
+# Documents
+
+#require_relative 'lifesigns_engine/objects/documents/test_email'
+#require_relative 'lifesigns_engine/objects/documents/test_journal'
+require_relative 'lifesigns_engine/objects/documents/test_sticky_note'
+
+# Keys
+
+#require_relative 'lifesigns_engine/objects/keys/test_metal_key'
+#require_relative 'lifesigns_engine/objects/keys/test_programmable_keycard'
+
+# Weapons
+
+#require_relative 'lifesigns_engine/objects/weapons/test_firearm'
+
+# End of File.

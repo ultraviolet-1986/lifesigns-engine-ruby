bc9d43e1f6dd24cc6d12c4fad5204ec62cefc02c
William Willis Whinn <w_willis86@hotmail.co.uk>
2017-10-10 21:08:50 +0100

    Update Style Sheet

--- lifesigns_engine/system/style_sheet.rb
+++ lifesigns_engine/system/style_sheet.rb
@@ -6,8 +6,9 @@
 
 class String
   def black;          "\e[30m#{self}\e[0m" end
-  def white;          "\e[97m#{self}\e[0m" end
   def gray;           "\e[37m#{self}\e[0m" end
+  def grey;           "\e[37m#{self}\e[0m" end
+  def white;          "\e[97m#{self}\e[0m" end
 
   def blue;           "\e[34m#{self}\e[0m" end
   def cyan;           "\e[36m#{self}\e[0m" end
@@ -25,8 +26,8 @@ class String
 
   def blink;          "\e[5m#{self}\e[25m" end
   def bold;           "\e[1m#{self}\e[22m" end
-  def italic;         "\e[3m#{self}\e[23m" end
   def invert;         "\e[7m#{self}\e[27m" end
+  def italic;         "\e[3m#{self}\e[23m" end
   def underline;      "\e[4m#{self}\e[24m" end
 end
 

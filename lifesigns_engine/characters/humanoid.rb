0a22006a69278ec4954148d6f02f9e70cefbce5c
William Whinn <w.willis.1986@gmail.com>
2018-04-03 10:56:13 +0100

    Include Health system for potential healing and harming

--- lifesigns_engine/characters/humanoid.rb
+++ lifesigns_engine/characters/humanoid.rb
@@ -1,15 +1,17 @@
 #!/usr/bin/env ruby
 
 class Humanoid
-  attr_reader :name, :description, :health_remaining, :inventory
+  attr_reader :name, :description, :maximum_health, :health_remaining, :inventory
 
-  def initialize(name, description, health_remaining, inventory)
+  def initialize(name, description, inventory)
     @name = name
     @description = description
-    @health_remaining = health_remaining
     @inventory = inventory
+    @maximum_health = 100
+    @health_remaining = 100
   end
 
+  def maximum_health=(maximum_health); @maximum_health = maximum_health end
   def health_remaining=(health_remaining); @health_remaining = health_remaining end
   def inventory=(inventory); @inventory = inventory end
 
@@ -23,6 +25,23 @@ class Humanoid
 
   def think(thought)
     puts_wrap("#{thought}")
+  end
+
+  def take_damage(damage_points)
+    self.health_remaining = self.health_remaining - damage_points
+
+    if self.health_remaining <= 0
+      puts "\nYou lost your life. Game over."
+      # TODO - Implement 'Game Over' logic here
+    end
+  end
+
+  def heal(health_points)
+    self.health_remaining = self.health_remaining + health_points
+
+    if self.health_remaining >= self.maximum_health
+      self.health_remaining = self.maximum_health
+    end
   end
 end
 
